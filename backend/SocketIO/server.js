4adaea6d18a082f326d810917bfe710ef60f7266
Varshney2309 <harshitvarshney3002@gmail.com>
2025-09-19 13:31:45 +0530

    Final changes

--- backend/SocketIO/server.js
+++ backend/SocketIO/server.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server,{
     cors:{
-        origin:"http://localhost:5173",
+        origin:" https://project-chatapp-3.onrender.com",
         methods:["GET","POST"]
     }
 })
